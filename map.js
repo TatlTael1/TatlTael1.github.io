4a61b18f1538b18bf0e84cd3d53d3cfbc1875949
TatlTael1 <darryl.chard@students.plymouth.ac.uk>
2017-03-15 22:50:41 +0000

    Edited method of Rendering

--- map.js
+++ map.js
@@ -1,21 +1,9 @@
 var i = 1;
+var pgons = {};
 
-function clearMap() {
-    for(i in map._layers) {
-        if(map._layers[i]._path != undefined) {
-            try {
-                map.removeLayer(map._layers[i]);
-                console.log(map._layers);
-            }
-            catch(e) {
-                console.log("problem with " + e + map._layers[i]);
-            }
-        }
-    }
-}
 
 map = L.map('map', { //Create the map and choose a center point and zoom level
-    center: [50.3755, -4.1427],
+    center: [50.3755, -4.1387],
     zoom: 17
 });
 
@@ -26,19 +14,22 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //Create a ti
 // create a red polygon from an array of LatLng points
 var latlngs = [[50.376429, -4.139996],[50.375889, -4.141241],[50.374989, -4.141680],[50.373850, -4.139950],[50.373800, -4.138550],[50.377089, -4.136150],[50.377089, -4.138673]];
 
-var latlngs2 = [[50.376729, -4.139996],[50.375889, -4.141241],[50.374989, -4.141680],[50.373850, -4.139950],[50.373800, -4.138550],[50.377089, -4.136150],[50.377089, -4.138673]];
+var latlngs2 = [[50.376229, -4.134996],[50.374989, -4.135761],[50.374759, -4.134861],[50.374950, -4.134450],[50.37590, -4.133950]];
+
+var lcontrol = L.control.layers(null, pgons).addTo(map);
 
 function switchRender() {
-    switch (document.querySelector('input[name="renderOption"]:checked').value) {
+    if(document.getElementById("polyName").value == "") {
+        alert("Please enter a name!");
+    } else {    
+        switch (document.querySelector('input[name="renderOption"]:checked').value) {
         case 'red':
             var polygon1 = L.polygon(latlngs, {color: 'red'});
             var polyNumber = "Polygon" + i;
-            var pgons = {
-                [polyNumber] : polygon1
-            };
+            pgons[polyNumber] = polygon1;
             //map.addLayer(polygon1);
-            var lcontrol = L.control.layers(pgons).addTo(map);
-            var pName = "Polygon " + i;
+            
+            var pName = document.getElementById("polyName").value;
             lcontrol.addOverlay(polygon1, pName);
             i++;
             console.log(map._layers);
@@ -47,24 +38,18 @@ function switchRender() {
             //clearMap();
             var polygon2 = L.polygon(latlngs2, {color: 'green'});
             var polyNumber = "Polygon" + i;
-            var pgons = {
-                [polyNumber] : polygon2
-            };
+            pgons[polyNumber] = polygon2;
             //map.addLayer(polygon1);
-            var lcontrol = L.control.layers(pgons).addTo(map);
-            var pName = "Polygon " + i;
+            var pName = document.getElementById("polyName").value;
             lcontrol.addOverlay(polygon2, pName);
             i++;
             console.log(map._layers);
             break;
-        case 'blank':
-            //clearMap();
-            if(map.polygon1 != undefined){
-                map.removeLayer(polygon1);
-            }
-            var polygon1 = L.polygon(latlngs, {color: '', opacity: 0, fillOpacity: 0}).addTo(map);
+        case 'pattern':
             break;
     }
-    // zoom the map to the polygon
-    map.fitBounds(polygon1.getBounds());
+        // zoom the map to the polygon
+        map.fitBounds(polygon1.getBounds());
+    }
+    
 }
